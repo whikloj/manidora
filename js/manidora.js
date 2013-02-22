7a41996665446919cbe6eae4c49004bf527d97de
mitchmac <mitchmac@gmail.com>
2013-02-22 13:15:44 -0400

    Add Full Screen link to compound navigation block

---
+++ js/manidora.js
@@ -0,0 +1,7 @@
+(function ($) {
+    Drupal.behaviors.manidora = {
+        attach: function (context, settings) {
+            $('.manidora-colorbox').colorbox({inline:true});
+        }
+    };
+})(jQuery);
