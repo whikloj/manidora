19a2492ae2e399e562101f503478a9b69c96539f
mitchmac <mitchmac@gmail.com>
2013-02-22 13:21:05 -0400

    Code style cleanup on new js file

--- js/manidora.js
+++ js/manidora.js
@@ -1,7 +1,11 @@
+/**
+ * @file
+ * Integrate with Colorbox.
+ */
 (function ($) {
-    Drupal.behaviors.manidora = {
-        attach: function (context, settings) {
-            $('.manidora-colorbox').colorbox({inline:true});
-        }
-    };
+  Drupal.behaviors.manidora = {
+    attach: function (context, settings) {
+      $('.manidora-colorbox').colorbox({inline:true});
+    }
+  };
 })(jQuery);
