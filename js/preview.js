b51469942933687912ba0f738a8aacb39af46e1b
Jared Whiklo <jared_whiklo@umanitoba.ca>
2014-03-14 11:40:38 -0500

    Made the preview window draggable to move it around incase it gets in the way.

---
+++ js/preview.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function() {
+  jQuery('#block-manidora-manidora-preview-image').css('left', (jQuery(window).width() - jQuery('#block-manidora-manidora-preview-image').width() - 20) + 'px').draggable();
+});
