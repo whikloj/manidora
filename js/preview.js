01566f3d14a0f98488778e2b0a6e54e8cb3b19cf
Jared Whiklo <jared_whiklo@umanitoba.ca>
2014-03-18 11:38:21 -0500

    Fix preview image, adding wait for slower images to load.

--- js/preview.js
+++ js/preview.js
@@ -1,3 +1,6 @@
 jQuery(document).ready(function() {
-  jQuery('#block-manidora-manidora-preview-image').css('left', (jQuery(window).width() - jQuery('#block-manidora-manidora-preview-image').width() - 20) + 'px').draggable();
+  // setTimeout because if the image is not loaded it will mess up the following css calculation
+  window.setTimeout(function(){
+    jQuery('#block-manidora-manidora-preview-image').css({'right': 'auto', 'left': (jQuery(window).width() - jQuery('#block-manidora-manidora-preview-image').width() - 20) + 'px'}).draggable();
+  }, 1000);
 });
